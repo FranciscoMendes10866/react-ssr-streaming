9a1911d1d92a6228be9cfb6849065f31cd6c7fa6
FranciscoMendes10866 <fmendes_dev@protonmail.ch>
2022-12-13 20:40:14 +0000

    added water.css

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ export const App = () => {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/dark.css" />
         <title>React SSR Streaming</title>
       </head>
       <body>
